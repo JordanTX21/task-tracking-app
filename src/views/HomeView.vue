ff11f84318e10a9096390ce4cd94433d5686115b
JordanTX21 <jordantx21@gmail.com>
2025-07-12 19:57:51 -0500

    correcciones frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,26 +18,60 @@ onMounted(() => {
   store.getTasks()
 })
 
+const getRoundedClasses = (index: number) => {
+  const len = users.value.length;
+
+  // Por breakpoint
+  const cols = {
+    'lg': 3,
+    'sm': 2,
+    'max-sm': 1,
+  };
+
+  const result: Record<string, boolean> = {};
+
+  Object.entries(cols).forEach(([prefix, colCount]) => {
+    const rowCount = Math.ceil(len / colCount);
+
+    const row = Math.floor(index / colCount);
+    const col = index % colCount;
+
+    const isFirstRow = row === 0;
+    const isLastRow = row === rowCount - 1;
+    const isFirstCol = col === 0;
+    const isLastCol = col === colCount - 1;
+
+    if (isFirstRow && isFirstCol) result[`rounded-tl-lg`] = true;
+    if (isFirstRow && isLastCol) result[`${prefix}:rounded-tr-lg`] = true;
+    if (isLastRow && isFirstCol) result[`${prefix}:rounded-bl-lg`] = true;
+    if (isLastRow && isLastCol) result[`${prefix}:rounded-br-lg`] = true;
+  });
+  console.log(result)
+
+  return result;
+};
+
 </script>
 
 <template>
   <div class="p-4 flex flex-col gap-y-4 h-screen" v-if="boards.length > 0 && users.length > 0">
     <div
       class="border border-slate-400 rounded-lg bg-slate-700 text-gray-300 text-xs grid lg:grid-cols-3 sm:grid-cols-2">
-      <RouterLink v-for="(user, index) in users" :key="`user-${index}`" :to="`/user/${user.id}`" :class="['py-2 px-4 flex gap-x-2 items-center cursor-pointer hover:bg-slate-500', {
-        'rounded-tl-lg': index === 0,
-        'rounded-tr-lg': index === 2,
-        'rounded-bl-lg': index === users.length - 3,
-        'rounded-br-lg': index === users.length - 1,
-      }]">
-        <Avatar :color="user.image ? 'stone' : get_color(user.name)"><img
-            :src="`https://erp.overskull.com${user.image}`" alt="" v-if="user.image"><span v-else>{{ get_abbr(user.name)
-            }}</span></Avatar>
-        <div class="font-medium">{{ capitalizeWords(user.name) }}</div>
-        <div class="font-medium">{{ user.done }} / {{ user.goal }}</div>
-        <div class="flex-1 rounded-lg bg-gray-300">
-          <div class="rounded-lg h-2 bg-red-600"
-            :style="{ 'width': `${(Math.min(user.done, user.goal) / user.goal) * 100}%` }"></div>
+      <RouterLink v-for="(user, index) in users" :key="`user-${index}`" :to="`/user/${user.id}`"
+        :class="['py-2 px-4 grid grid-cols-2 gap-x-2 items-center cursor-pointer hover:bg-slate-500', getRoundedClasses(index)]">
+        <div class="flex items-center gap-x-2">
+          <Avatar :color="user.image ? 'stone' : get_color(user.name)"><img
+              :src="`https://erp.overskull.com${user.image}`" alt="" v-if="user.image"><span v-else>{{
+                get_abbr(user.name)
+              }}</span></Avatar>
+          <div class="font-medium">{{ capitalizeWords(user.name) }}</div>
+        </div>
+        <div class="flex flex-col gap-y-2">
+          <div class="font-medium text-center">{{ user.done }} / {{ user.goal }}</div>
+          <div class="flex-1 rounded-lg bg-gray-300">
+            <div class="rounded-lg h-2 bg-red-600"
+              :style="{ 'width': `${(Math.min(user.done, user.goal) / user.goal) * 100}%` }"></div>
+          </div>
         </div>
       </RouterLink>
     </div>
